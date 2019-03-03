de649444be9c08e6f17b6bcc38b6382dff313635
BartekTricks <bartektricks@gmail.com>
2019-03-03 16:03:48 +0100

    RWD

--- src/components/Hero/Hero.style.js
+++ src/components/Hero/Hero.style.js
@@ -1,15 +1,20 @@
 import { string, func, oneOfType, oneOf, any } from 'prop-types';
 import styled, { keyframes, css } from 'styled-components';
 import { rem, rgba } from 'polished';
-import { variables, colors } from '../../global/helpers';
+import { variables, utils } from '../../global/helpers';
 import { containerStyles } from '../../global/commonStyles';
 
-const { fontSize, fontFamily, marginSize, globalSize } = variables;
+const { fontSize, fontFamily, marginSize, globalSize, breakpoints } = variables;
+const { media } = utils;
 
 export const HeroHeader = styled.header`
   ${containerStyles};
   
-    padding: ${rem(globalSize.paddingSmall)} ${rem(globalSize.gutter)} 0;
+  padding: ${rem('30px')} ${rem(globalSize.gutter)} 0;
+  
+  ${media.greaterThan(breakpoints.landscape)`
+    padding: ${rem('83px')} ${rem(globalSize.gutter)} 0;
+  `}
 `;
 
 export const ButtonWrapper = styled.div`
@@ -18,9 +23,18 @@ export const ButtonWrapper = styled.div`
   max-width: ${rem('200px')};
   margin: 0 auto;
   
+  ${media.greaterThan(breakpoints.tablet)`
+    flex-direction: row;
+    max-width: ${rem('400px')};
+  `}
+  
   & > button {
-    &:first-child {
-      margin: 0 auto 120px;
+    &:first-child { 
+      margin: 0 auto ${rem(marginSize.small)};
+      
+      ${media.greaterThan(breakpoints.tablet)`
+        margin: 0 ${rem('20px')} 0 auto;
+      `}
     }
   }
 `;
--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { string, func, oneOfType, oneOf, any } from 'prop-types';
 import styled, { keyframes, css } from 'styled-components';
 import { rem, rgba } from 'polished';
-import { variables, colors } from '../../global/helpers';
+import { variables, colors, utils } from '../../global/helpers';
 
-const { fontSize, fontFamily, marginSize, globalSize } = variables;
+const { fontSize, fontFamily, marginSize, globalSize, breakpoints } = variables;
+const { media } = utils;
 
 const Typography = ({
   component: Component,
@@ -15,11 +16,18 @@ const Typography = ({
 }) => {
   const mobileHeading = css`
     font-size: ${fontSize.mediumHeading};
-    margin: 0 0 ${marginSize.regular};
+    margin: 0 auto ${marginSize.regular};
   `;
 
   const isHero = css`
+    max-width: ${rem('560px')};
     ${mobileHeading}
+    
+    ${media.greaterThan(breakpoints.landscape)`
+      font-size: ${fontSize.heroHeading};
+      line-height: 1.27;
+      margin: 0 auto ${marginSize.small};
+    `}
   `;
 
   const isHeading = css`
