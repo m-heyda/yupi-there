19eadd26c7a651669afc3191e35557022e404be9
Mariusz <insanepl@gmail.com>
2019-03-03 12:28:46 +0100

    Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
+import React  from 'react';
+import { string, bool, func, oneOfType, any } from 'prop-types';
+import cn from 'classnames';
 
-class Button extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <button>
-          Button
-        </button>
-      </React.Fragment>
-    );
-  }
-}
+const Button = ({
+  component: Component,
+  secondary,
+  onClick,
+  children,
+  ...props
+}) => {
+  const buttonClasses = cn('button', {'is-secondary': secondary});
+
+  return (
+    <Component
+      className={buttonClasses}
+      onClick={e => {onClick(e)}}
+      {...props}
+    >
+      <span className='button-inner-text'>
+        {children}
+      </span>
+    </Component>
+  );
+};
+
+Button.defaultProps = {
+  component: 'button',
+};
+
+Button.propTypes = {
+  onClick: func,
+  component: oneOfType([string, func]).isRequired,
+  children: any.isRequired,
+  secondary: bool,
+};
 
 export default Button;
