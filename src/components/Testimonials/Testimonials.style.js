c35dc1103dd91eb5a251b612c616a6afe90dc1b6
Mariusz <insanepl@gmail.com>
2019-03-20 20:53:40 +0100

    reformat

--- src/components/Testimonials/Testimonials.style.js
+++ src/components/Testimonials/Testimonials.style.js
@@ -15,7 +15,7 @@ export const Container = styled.div`
 export const Wrapper = styled.section`
   padding: ${rem(globalSize.paddingSmall)} 0;
   border-bottom: 1px solid ${colors.athensGray};
-  
+
   ${media.greaterThan('tablet')`
     padding: ${rem(globalSize.paddingMedium)} 0;
   `}
@@ -23,12 +23,12 @@ export const Wrapper = styled.section`
 
 export const TestimonialsWrapper = styled.div`
   margin: ${rem('40px')} -${rem(globalSize.gutter)};
-  
+
   ${media.greaterThan('tablet')`
     margin: ${rem('60px')} 0;
     padding: 0;
   `}
-  
+
   ${media.greaterThan('desktop')`
     display: flex;
     justify-content: space-between;
@@ -42,8 +42,8 @@ export const TestimonialBox = styled.div`
   background: ${colors.titanWhite};
   border: 1px solid ${colors.athensGray};
   border-radius: ${rem(6)};
-  
-   ${media.greaterThan('desktop')`
+
+  ${media.greaterThan('desktop')`
     flex: 0 0 49%;
     max-width: 49%;
     padding: ${rem('40px')};
@@ -57,7 +57,7 @@ export const TestimonialBox = styled.div`
 export const TestimonialIcon = styled(QuoteIcon)`
   display: block;
   margin: 0 0 ${rem('22px')};
-  
+
   ${media.greaterThan('desktop')`
     margin: 0 0 ${rem('32px')};
   `}
