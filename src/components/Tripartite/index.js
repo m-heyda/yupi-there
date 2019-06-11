a6cd5805116fa780ab2836fccacd0e873a70ddc6
Maciej Sykuła <maciek.sykula@gmail.com>
2019-06-11 08:46:00 +0200

    Add Tripartite component

---
+++ src/components/Tripartite/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import TwoColumnsLayout from '../TwoColumnsLayout';
+
+import {
+  ParagraphText,
+  Container,
+  Image,
+  Title,
+  Header,
+} from './Tripartite.style';
+
+const Item = ({ icon, title, text }) => (
+  <div>
+    <Header>
+      <Image src={ icon } />
+      <Title>{ title }</Title>
+    </Header>
+    <ParagraphText>{ text }</ParagraphText>
+  </div>
+)
+
+const Tripartite = ({ content }) => {
+  const columnsItems = content.slice(0, 2);
+  const restItems = content.slice(2, content.length);
+
+  return (
+    <section>
+      <TwoColumnsLayout
+        leftChildren={ <Item { ...columnsItems[0] }/> }
+        rightChildren={ <Item { ...columnsItems[1] }/> }
+        centerBottomChildren={ restItems.map(item => <Item { ...item } />)}
+      />
+    </section>
+  );
+};
+
+export default Tripartite;
