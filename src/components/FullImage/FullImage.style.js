ce624e42be4e7dcd990d6e76ebd3c0277cc56114
Maciej Sykuła <maciek.sykula@gmail.com>
2019-06-11 22:46:25 +0200

    Add FullImage component

---
+++ src/components/FullImage/FullImage.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { rem, rgba } from 'polished';
+import { colors } from '../../global/helpers';
+import { containerStyles, Paragraph } from '../../global/commonStyles';
+import { variables, utils } from '../../global/helpers';
+
+const { globalSize, marginSize, transitions } = variables;
+const { media } = utils;
+
+export const Wrapper = styled.div`
+  ${containerStyles};
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+`;
