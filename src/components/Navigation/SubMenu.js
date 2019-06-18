21a0aa18900c8131fcb6919f674ab0266ca3a674
Maciej Sykuła <maciek.sykula@gmail.com>
2019-06-18 07:42:09 +0200

    Update Navigation clickable area and trailing slashes

--- src/components/Navigation/SubMenu.js
+++ src/components/Navigation/SubMenu.js
@@ -5,27 +5,19 @@ const SubMenu = () => {
   return (
     <SubMenuWrapper>
       <SubMenuItem>
-        <SubMenuLink href='/reklamy-google'>Google Ads</SubMenuLink>
+        <SubMenuLink href='/reklamy-google/'>Google Ads</SubMenuLink>
       </SubMenuItem>
 
       <SubMenuItem>
-        <SubMenuLink href='/oferta-seo'>SEO</SubMenuLink>
+        <SubMenuLink href='/oferta-seo/'>SEO</SubMenuLink>
       </SubMenuItem>
 
       <SubMenuItem>
-        <SubMenuLink href='/analityka'>Analityka Web</SubMenuLink>
+        <SubMenuLink href='/analityka/'>Analityka Web</SubMenuLink>
       </SubMenuItem>
 
       <SubMenuItem>
-        <SubMenuLink href='/kampanie-sm'>Kampanie social media</SubMenuLink>
-      </SubMenuItem>
-
-      <SubMenuItem>
-        <SubMenuLink href='#'>Grafika Online</SubMenuLink>
-      </SubMenuItem>
-
-      <SubMenuItem>
-        <SubMenuLink href='#'>Content Marketing</SubMenuLink>
+        <SubMenuLink href='/kampanie-sm/'>Kampanie social media</SubMenuLink>
       </SubMenuItem>
     </SubMenuWrapper>
   );
